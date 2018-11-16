f41abc240e85f57367ea40846efe2b612899e7a9
Progroveriman <clxfxmk@qq.com>
2018-11-15 19:39:15 -0500

    Create 104.java
    As titled

---
+++ 104.java
@@ -0,0 +1,80 @@
+import java.util.LinkedList;
+import java.util.Stack;
+
+/*
+Given a binary tree, find its maximum depth.
+
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Given binary tree [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its depth = 3.
+* */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        return helper(root.left, root.right);
+    }
+
+    private boolean helper(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+
+        if (left == null || right == null) {
+            return false;
+        }
+
+        if (left.val != right.val) {
+            return false;
+        }
+
+        return helper(left.left, right.right) && helper(left.right, right.left);
+    }
+    public int maxDepth(TreeNode root) {
+
+        if (root == null)
+            return 0;
+
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
+public class firstTest{
+    public static void main(String[] args){
+        Solution s = new Solution();
+
+        TreeNode t1 = new TreeNode(3);
+        TreeNode t2 = new TreeNode(9);
+        TreeNode t3 = new TreeNode(20);
+        TreeNode t4 = new TreeNode(15);
+        TreeNode t5 = new TreeNode(7);
+
+        t1.left = t2;
+        t1.right = t3;
+        t2.left = null;
+        t2.right = null;
+        t3.left = t4;
+        t3.right = t5;
+        t4.left = t4.right = t5.left = t5.right = null;
+        System.out.print(s.maxDepth(t1));
+
+    }
+}
