ee78321b26633d3bd569a1735934df59d3202ac5
Progroveriman <clxfxmk@qq.com>
2021-03-03 22:53:24 -0500

    Some new problems

--- py-Jindian/17.14.py
+++ py-Jindian/17.14.py
@@ -16,3 +16,36 @@
 	0 <= k <= min(100000, len(arr))
 
 '''
+import heapq
+
+class Solution:
+    def smallestK1(self, arr, k):
+        res =[]
+        if k == 0:
+            return res
+        heap = [-x for x in arr[:k]]
+        heapq.heapify(heap)
+        for i in range(k, len(arr)):
+            if arr[i] < -heap[0]:
+                heapq.heappop(heap)
+                heapq.heappush(heap, -arr[i])
+        res = [-x for x in heap]
+        return res
+
+    def smallestK(self, arr, k):
+        res = []
+        if k == 0:
+            return res
+        heap = [x for x in arr]
+        for i, j in zip(range(len(arr)), range(k)):
+            heapq.heapify(heap)
+            res.append(heap.pop(0))
+        return res
+
+
+print(Solution().smallestK([1,3,5,7,2,4,6,8], 4))
+
+# 作者：LionKing865
+# 链接：https://leetcode-cn.com/problems/smallest-k-lcci/solution/python3-zui-da-dui-1714-by-lionking865-z4nu/
+# 来源：力扣（LeetCode）
+# 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
