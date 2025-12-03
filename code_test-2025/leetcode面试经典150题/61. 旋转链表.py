6363364b2ed6a278169834d7828e593cf8a6eed7
Minke Xiu <xmk233@163.com>
2025-12-03 22:14:11 +0800

    some new code

---
+++ code_test-2025/leetcode面试经典150题/61. 旋转链表.py
@@ -0,0 +1,34 @@
+'''中等
+相关标签
+premium lock icon
+相关企业
+给你一个链表的头节点 head ，旋转链表，将链表每个节点向右移动 k 个位置。
+
+ 
+
+示例 1：
+
+
+输入：head = [1,2,3,4,5], k = 2
+输出：[4,5,1,2,3]
+示例 2：
+
+
+输入：head = [0,1,2], k = 4
+输出：[2,0,1]
+ 
+
+提示：
+
+链表中节点的数目在范围 [0, 500] 内
+-100 <= Node.val <= 100
+0 <= k <= 2 * 109'''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        
